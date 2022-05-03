db4f29e9b7be4765092aa0043dfb8b676a40e31e
Wilco Boshoff <wlboshoff@gmail.com>
2022-05-03 06:02:25 +0200

    Added SPA dev utilities
    Added bootstrap, prettier, eslint and storybook.

---
+++ src/spa/src/app/app.component.stories.ts
@@ -0,0 +1,8 @@
+import { Meta, Story } from '@storybook/angular';
+import { AppComponent } from './app.component';
+
+export default <Meta>{
+  component: AppComponent
+};
+
+export const Primary: Story = () => ({});
